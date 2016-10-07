d1a3237dc04a3ef0ec1a34b18f2f7bdaf0d3981f
Matt Watson <mattdangerw@gmail.com>
2016-10-06 17:26:23 -0700

    Rewrite to use flatpak-builder

--- index.js
+++ index.js
@@ -2,30 +2,35 @@
 
 const _ = require('lodash')
 const fs = require('fs-extra')
-const ini = require('ini')
 const path = require('path')
+const quote = require('shell-quote').quote
 const util = require('util')
 
 const pkg = require('./package.json')
 const logger = require('debug')(pkg.name)
 
 const promisify = require('es6-promisify')
-const readFile = promisify(fs.readFile)
 const writeFile = promisify(fs.writeFile)
-const copy = promisify(fs.copy)
-const move = promisify(fs.move)
-const remove = promisify(fs.remove)
-const symlink = promisify(fs.symlink)
-const readdir = promisify(fs.readdir)
 const mkdirs = promisify(fs.mkdirs)
-const recursiveReaddir = promisify(require('recursive-readdir'))
 const exec = promisify(require('child_process').exec, { multiArgs: true })
 const tmpdir = promisify(require('tmp').dir)
 
-function execAndLog (command) {
-  logger(command)
-  return exec(command)
+function kebabify (object) {
+  return _.cloneDeepWith(object, (value) => {
+    if (!value || typeof value !== 'object') return value
+
+    return Object.keys(value).reduce(function (newValue, key) {
+      newValue[_.kebabCase(key)] = value[key]
+      return newValue
+    }, {})
+  })
+}
+
+function execAndLog (options, args) {
+  let command = quote(args)
+  return exec(command, { cwd: options['working-dir'] })
     .then(function (output) {
+      logger(command)
       if (output[0]) logger('stdout ->\n' + output[0])
       if (output[1]) logger('stderr ->\n' + output[1])
     })
@@ -34,215 +39,157 @@ function execAndLog (command) {
 function addCommandLineOption (args, name, value) {
   if (!value) return
 
+  if (value === true) {
+    args.push(util.format('--%s', name))
+    return
+  }
+
   args.push(util.format('--%s=%s', name, value))
 }
 
 function getOptionsWithDefaults (options) {
-  return _.defaults({}, options, {
-    branch: 'master',
-    sdk: 'org.freedesktop.Sdk',
-    runtime: 'org.freedesktop.Platform',
-    finishArgs: [],
-    symlinks: [],
-    renameFiles: true
-  })
+  let defaults = {
+    'build-dir': path.join(options['working-dir'], 'build'),
+    'repo-dir': path.join(options['working-dir'], 'repo'),
+    'manifest-path': path.join(options['working-dir'], 'manifest.json'),
+    'extra-flatpak-builder-args': [],
+    'extra-flatpak-build-bundle-args': []
+  }
+  options = _.defaults({}, options, defaults)
+  options['working-dir'] = path.resolve(options['working-dir'])
+  options['build-dir'] = path.resolve(options['build-dir'])
+  options['repo-dir'] = path.resolve(options['repo-dir'])
+  options['manifest-path'] = path.resolve(options['manifest-path'])
+  options['bundle-path'] = path.resolve(options['bundle-path'])
+  return options
 }
 
-function ensureDirectories (options) {
-  let dirs = []
-  if (!options.buildDir) {
-    let makeDir = tmpdir()
+function getManifestWithDefaults (manifest) {
+  let defaults = {
+    'branch': 'master',
+    'sdk': 'org.freedesktop.Sdk',
+    'runtime': 'org.freedesktop.Platform',
+    'modules': [],
+    'files': [],
+    'symlinks': []
+  }
+  if (typeof manifest['runtime'] === 'undefined') {
+    defaults['runtime-version'] = '1.4'
+  }
+  return _.defaults({}, manifest, defaults)
+}
+
+function addManfiestFilesAndLinks (options, manifest) {
+  let commands = []
+  for (let sourceDest of manifest['files']) {
+    let source = path.resolve(sourceDest[0])
+    let dest = path.join('/app', sourceDest[1])
+    let dir = dest
+    if (!_.endsWith(dir, path.sep)) dir = path.dirname(dir)
+    commands.push(quote(['mkdir', '-p', dir]))
+    commands.push(quote(['cp', '-r', source, dest]))
+  }
+  for (let targetDest of manifest['symlinks']) {
+    let target = path.join('/app', targetDest[0])
+    let dest = path.join('/app', targetDest[1])
+    let dir = path.dirname(dest)
+    commands.push(quote(['mkdir', '-p', dir]))
+    commands.push(quote(['ln', '-s', target, dest]))
+  }
+  // This is kinda gross, but at the moment flatpak won't accept a source that
+  // includes no make or cmake files. Include a minimal Makefile
+  commands.push('echo "all:\ninstall:\n" > Makefile')
+  let module = {
+    'name': [pkg.name, 'files'].join('-'),
+    'no-autogen': true,
+    'build-options': {
+      'build-args': [
+        '--filesystem=' + options['working-dir'],
+        '--filesystem=host'
+      ]
+    },
+    'sources': [
+      {
+        'type': 'shell',
+        'commands': commands
+      }
+    ]
+  }
+  manifest['modules'].push(module)
+  delete manifest['files']
+  delete manifest['symlinks']
+}
+
+function ensureWorkingDir (options) {
+  if (!options['working-dir']) {
+    return tmpdir({ dir: '/var/tmp', unsafeCleanup: true })
       .then(function (dir) {
-        options.buildDir = dir
+        options['working-dir'] = dir
       })
-    dirs.push(makeDir)
   } else {
-    dirs.push(remove(options.buildDir))
+    return mkdirs(options['working-dir'])
   }
-  if (!options.repoDir) {
-    let makeDir = tmpdir()
-      .then(function (dir) {
-        options.repoDir = dir
-      })
-    dirs.push(makeDir)
-  }
-  return Promise.all(dirs)
 }
 
-function flatpakBuildInit (options) {
-  let args = ['flatpak build-init']
-  addCommandLineOption(args, 'arch', options.arch)
-  args.push(options.buildDir)
-  args.push(options.id)
-  args.push(options.sdk)
-  args.push(options.runtime)
-  args.push(options.runtimeVersion)
-  return execAndLog(args.join(' '))
+function writeJsonFile (options, manifest) {
+  return writeFile(options['manifest-path'], JSON.stringify(manifest, null, '  '))
 }
 
-function copyInFiles (options) {
-  let copies = _.map(options.files, function (item) {
-    let source = item[0]
-    let dest = path.join(options.buildDir, 'files', item[1])
-    let destDir = dest.substring(0, dest.lastIndexOf(path.sep))
-    logger('Copying ' + source + ' -> ' + dest)
-    return mkdirs(destDir)
-      .then(function () {
-        return copy(source, dest)
-      })
-  })
-  return Promise.all(copies)
+function flatpakBuilder (options) {
+  let args = ['flatpak-builder']
+  addCommandLineOption(args, 'arch', options['arch'])
+  addCommandLineOption(args, 'gpg-sign', options['gpg-sign'])
+  addCommandLineOption(args, 'gpg-homedir', options['gpg-homedir'])
+  addCommandLineOption(args, 'subject', options['subject'])
+  addCommandLineOption(args, 'body', options['body'])
+  addCommandLineOption(args, 'repo', options['repo-dir'])
+  addCommandLineOption(args, 'force-clean', true)
+  args.concat(options['extra-flatpak-builder-args'])
+
+  args.push(options['build-dir'])
+  args.push(options['manifest-path'])
+  return execAndLog(options, args)
 }
 
-function renameFiles (options) {
-  if (!options.renameFiles) return
+function flatpakBuildBundle (options, manifest) {
+  if (!options['bundle-path']) return
 
-  let applicationsDir = path.join(options.buildDir, 'files', 'share', 'applications')
-  let iconsDir = path.join(options.buildDir, 'files', 'share', 'icons')
+  let args = ['flatpak', 'build-bundle']
+  addCommandLineOption(args, 'arch', options['arch'])
+  addCommandLineOption(args, 'gpg-sign', options['gpg-sign'])
+  addCommandLineOption(args, 'gpg-homedir', options['gpg-homedir'])
+  addCommandLineOption(args, 'repo-url', options['bundle-repo-url'])
+  if (options['build-runtime']) addCommandLineOption(args, 'runtime', true)
+  args.concat(options['extra-flatpak-build-bundle-args'])
 
-  function findDesktopFile () {
-    return readdir(applicationsDir)
-      .then(function (desktopPaths) {
-        desktopPaths = desktopPaths.filter(function (desktopPath) {
-          return path.extname(desktopPath) === '.desktop'
-        })
-        if (desktopPaths.length !== 1) return
-
-        return path.join(applicationsDir, desktopPaths[0])
-      }).catch(function (error) {
-        if (error.code !== 'ENOENT') throw error
-      })
-  }
-
-  function renameDesktopFile (desktopPath) {
-    if (!desktopPath) return
-    let newDesktopPath = path.join(applicationsDir, options.id + '.desktop')
-    if (desktopPath === newDesktopPath) return
-
-    logger('Renaming desktop file ' + desktopPath + ' -> ' + newDesktopPath)
-    return move(desktopPath, newDesktopPath)
-      .then(function () {
-        return newDesktopPath
-      })
-  }
-
-  function rewriteDesktopFile (desktopPath) {
-    if (!desktopPath) return
-
-    return readFile(desktopPath, 'utf-8')
-      .then(function (contents) {
-        let data = ini.parse(contents)
-        if (!('Desktop Entry' in data)) return
-        let iconName = data['Desktop Entry']['Icon']
-        if (iconName === options.id) return
-        contents = contents.replace('Icon=' + iconName, 'Icon=' + options.id)
-        return writeFile(desktopPath, contents).then(function () {
-          return iconName
-        })
-      })
-  }
-
-  function renameIcons (iconName) {
-    if (!iconName) return
-
-    return recursiveReaddir(iconsDir)
-      .then(function (iconPaths) {
-        let moves = []
-        _.map(iconPaths, function (iconPath) {
-          let dir = path.dirname(iconPath)
-          let oldname = path.basename(iconPath)
-          let newname = oldname.replace(iconName, options.id)
-          if (newname === oldname) return
-          let newPath = path.join(dir, newname)
-          logger('Renaming icon file ' + iconPath + ' -> ' + newPath)
-          moves.push(move(iconPath, newPath))
-        })
-        return Promise.all(moves)
-      })
-  }
-
-  return findDesktopFile()
-    .then(renameDesktopFile)
-    .then(rewriteDesktopFile)
-    .then(renameIcons)
-}
-
-function createSymlinks (options) {
-  let links = _.map(options.symlinks, function (item) {
-    let target = path.join('/app', item[0])
-    let linkpath = path.join(options.buildDir, 'files', item[1])
-    let dir = path.dirname(linkpath)
-    return mkdirs(dir)
-      .then(function () {
-        symlink(target, linkpath)
-      })
-  })
-  return Promise.all(links)
-}
-
-function flatpakBuildFinish (options) {
-  let args = ['flatpak build-finish']
-  addCommandLineOption(args, 'command', options.command)
-  args = args.concat(options.finishArgs)
-  args.push(options.buildDir)
-  return execAndLog(args.join(' '))
-}
-
-function flatpakBuildExport (options) {
-  let args = ['flatpak build-export']
-  addCommandLineOption(args, 'arch', options.arch)
-  addCommandLineOption(args, 'gpg-sign', options.gpgSign)
-  addCommandLineOption(args, 'gpg-homedir', options.gpgHomedir)
-  addCommandLineOption(args, 'subject', options.subject)
-  addCommandLineOption(args, 'body', options.body)
-  args.push(options.repoDir)
-  args.push(options.buildDir)
-  args.push(options.branch)
-  return execAndLog(args.join(' '))
-}
-
-function flatpakBuildBundle (options) {
-  if (!options.bundlePath) return
-
-  let args = ['flatpak build-bundle']
-  addCommandLineOption(args, 'arch', options.arch)
-  addCommandLineOption(args, 'gpg-sign', options.gpgSign)
-  addCommandLineOption(args, 'gpg-homedir', options.gpgHomedir)
-  args.push(options.repoDir)
-  args.push(options.bundlePath)
-  args.push(options.id)
-  args.push(options.branch)
-  return mkdirs(path.dirname(options.bundlePath))
+  args.push(options['repo-dir'])
+  args.push(options['bundle-path'])
+  args.push(manifest['id'])
+  args.push(manifest['branch'])
+  return mkdirs(path.dirname(options['bundle-path']))
     .then(function () {
-      return execAndLog(args.join(' '))
+      return execAndLog(options, args)
     })
 }
 
-exports.bundle = function (options, callback) {
-  if (!options.id) {
-    callback(new Error('You need to specify an application id.'))
-    return
-  }
-  if (!options.files) {
-    callback(new Error('You need to specify some application files.'))
-    return
-  }
+exports.bundle = function (manifest, options, callback) {
+  manifest = kebabify(manifest)
+  options = kebabify(options)
 
-  options = getOptionsWithDefaults(options)
+  return ensureWorkingDir(options)
+    .then(() => {
+      options = getOptionsWithDefaults(options)
+      manifest = getManifestWithDefaults(manifest)
+      addManfiestFilesAndLinks(options, manifest)
 
-  ensureDirectories(options)
-    .then(function (options) {
-      logger('Using arguments ->\n' + JSON.stringify(options, null, '  '))
+      logger('Using manifest ->\n' + JSON.stringify(manifest, null, '  '))
+      logger('Using options ->\n' + JSON.stringify(options, null, '  '))
     })
-    .then(() => flatpakBuildInit(options))
-    .then(() => copyInFiles(options))
-    .then(() => renameFiles(options))
-    .then(() => createSymlinks(options))
-    .then(() => flatpakBuildFinish(options))
-    .then(() => flatpakBuildExport(options))
-    .then(() => flatpakBuildBundle(options))
+    .then(() => writeJsonFile(options, manifest))
+    .then(() => flatpakBuilder(options))
+    .then(() => flatpakBuildBundle(options, manifest))
     .then(function () {
-      callback(null, options)
+      callback(null, options, manifest)
     }, function (error) {
       callback(error)
     })
