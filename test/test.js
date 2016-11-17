9aebe79f2556b78357e5ef40e1da81999222eae3
Matt Watson <mattdangerw@gmail.com>
2016-11-17 11:38:23 -0800

    test: add a few more unit tests https://phabricator.endlessm.com/T13989

--- test/test.js
+++ test/test.js
@@ -9,25 +9,23 @@ const rimraf = require('rimraf')
 
 const outputPath = path.join(__dirname, 'out')
 const refsPath = path.join(__dirname, '..', 'refs')
+const binPath = path.join(outputPath, 'hello')
+const flatpakPath = path.join(outputPath, 'hello.flatpak')
+const runtimeRefPath = path.join(refsPath, 'freedesktop-runtime-1.4.flatpakref')
 
 describe('flatpak-bundler', function () {
   describe('bundle', function () {
     this.timeout(30000)
 
-    before(function (done) {
+    beforeEach(function (done) {
       rimraf(outputPath, done)
       fs.mkdirSync(outputPath)
-    })
-
-    it('creates a flatpak', function (done) {
-      const binPath = path.join(outputPath, 'hello')
-      const flatpakPath = path.join(outputPath, 'hello.flatpak')
-      const runtimeRefPath = path.join(refsPath, 'freedesktop-runtime-1.4.flatpakref')
-
       fs.writeFileSync(binPath,
       `#!/bin/bash
       echo "Hello, world!"`, { mode: 0o755 })
+    })
 
+    it('creates a flatpak', function (done) {
       flatpakBundler.bundle({
         id: 'org.world.Hello',
         runtime: 'org.freedesktop.Platform',
@@ -46,7 +44,46 @@ describe('flatpak-bundler', function () {
       })
     })
 
-    after(function (done) {
+    it('accepts dash variants', function (done) {
+      flatpakBundler.bundle({
+        'id': 'org.world.Hello',
+        'runtime': 'org.freedesktop.Platform',
+        'runtime-version': '1.4',
+        'runtime-flatpakref': runtimeRefPath,
+        'sdk': 'org.freedesktop.Sdk',
+        'files': [
+          [binPath, '/bin/hello']
+        ]
+      }, {
+        'bundle-path': flatpakPath
+      }, function (error) {
+        if (error) return done(error)
+        assert(fs.existsSync(flatpakPath))
+        done()
+      })
+    })
+
+    it('recognizes a node style arch', function (done) {
+      flatpakBundler.bundle({
+        id: 'org.world.Hello',
+        runtime: 'org.freedesktop.Platform',
+        runtimeVersion: '1.4',
+        runtimeFlatpakref: runtimeRefPath,
+        sdk: 'org.freedesktop.Sdk',
+        files: [
+          [binPath, '/bin/hello']
+        ]
+      }, {
+        arch: 'x64',
+        bundlePath: flatpakPath
+      }, function (error) {
+        if (error) return done(error)
+        assert(fs.existsSync(flatpakPath))
+        done()
+      })
+    })
+
+    afterEach(function (done) {
       rimraf(outputPath, done)
     })
   })
